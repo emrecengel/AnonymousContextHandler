25be2a3ad5f8cdfa7b0d8569dcf2bb79eb36c882
Emre Cengel <ecengel@comwebinc.net>
2016-04-04 14:55:08 -0400

    Clean up

--- AnonymousContextHandler/Root/IEntityFrameWorkContext.cs
+++
@@ -1,7 +0,0 @@
-namespace AnonymousContextHandler.Root
-{
-    internal interface IDataBaseContext<T> : IContext<T> where T : class
-    {
-        void SetConnectionString(string connectionString);
-    }
-}
